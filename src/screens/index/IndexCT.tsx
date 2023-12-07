ab9bb40778c68308440e15e7c436966e75a76bab
김영선 <withbbang@gmail.com>
2023-12-07 17:55:43 +0900

    Test: post api confirm popup test

--- src/screens/index/IndexCT.tsx
+++ src/screens/index/IndexCT.tsx
@@ -1,9 +1,23 @@
 import React, { useEffect } from 'react';
 import { CommonState } from 'middlewares/reduxToolkits/commonSlice';
 import { CustomWindow } from 'modules/types';
+import { usePostDataByConfirmPopupHook } from 'modules/customHooks';
 import IndexPT from './IndexPT';
 
 function IndexCT({}: typeIndexCT): React.JSX.Element {
+  const { useSetActivePostDataByConfirmPopup } =
+    usePostDataByConfirmPopupHook();
+  const onClick = () => {
+    useSetActivePostDataByConfirmPopup({
+      message: 'hello',
+      url: '/wlekfj',
+      params: {},
+      successCb: () => console.warn('called successCb'),
+      cancelCb: () => console.warn('called cancelCb'),
+      failCb: () => console.warn('called failCb'),
+      errorPopupBtnCb: () => console.warn('called errorPopupBtnCb'),
+    });
+  };
   useEffect(() => {
     const customWindow = window as CustomWindow;
     customWindow.goBack = handleGoBack;
@@ -23,7 +37,7 @@ function IndexCT({}: typeIndexCT): React.JSX.Element {
     console.warn('onResult visit?', data);
   };
 
-  return <IndexPT />;
+  return <IndexPT onClick={onClick} />;
 }
 
 interface typeIndexCT extends CommonState {}
--- src/screens/index/IndexPT.tsx
+++ src/screens/index/IndexPT.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import styles from './Index.module.scss';
 
-function IndexPT({}: typeIndexPT): React.JSX.Element {
+function IndexPT({ onClick }: typeIndexPT): React.JSX.Element {
   return (
     <div className={styles.wrap}>
-      <h1>Index Page</h1>
+      <h1 onClick={onClick}>Index Page</h1>
     </div>
   );
 }
 
-interface typeIndexPT {}
+interface typeIndexPT {
+  onClick: () => void;
+}
 
 export default IndexPT;
