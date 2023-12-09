b6e2abcac4d3d4596d355434820a589187ea8f77
Bread-Kim <withbbang@gmail.com>
2023-12-09 14:13:03 +0900

    Refactor: 커스텀 훅 복수 데이터 호출 훅들 제거

--- src/modules/customHooks.ts
+++ src/modules/customHooks.ts
@@ -45,66 +45,7 @@ export function useSetCatchClauseForErrorPopupHook() {
 }
 
 /**
- * [get method 커스텀 훅 (배열 값 호출)]
- *
- * @param {TypeGetAPIHookParams} params
- * url, API 성공시 바로 실행하는 콜백, API 실패시 바로 실행하는 콜백, 에러팝업 버튼 콜백을 담고 있는 객체
- *
- * @returns
- */
-export function useGetDatasHook({
-  url,
-  successCb,
-  failCb,
-  errorPopupBtnCb,
-}: TypeGetAPIHookParams) {
-  const dispatch = useDispatch();
-  const useSetCatchClauseForErrorPopup = useSetCatchClauseForErrorPopupHook();
-  const [datas, setDatas] = useState<any[]>([]); // FIXME: 수정 필요
-
-  useEffect(() => {
-    (async () => {
-      if (!url) return;
-
-      try {
-        dispatch(useSetIsLoading({ isLoading: true }));
-        setDatas(await getAPI(url, failCb)); // FIXME: 수정 필요
-        successCb?.();
-      } catch (error: any) {
-        useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
-      } finally {
-        dispatch(useSetIsLoading({ isLoading: false }));
-      }
-    })();
-  }, [url]);
-
-  const useGetDatas = useCallback(
-    async ({
-      url,
-      successCb,
-      failCb,
-      errorPopupBtnCb,
-    }: TypeGetAPIHookParams) => {
-      if (!url) return;
-
-      try {
-        dispatch(useSetIsLoading({ isLoading: true }));
-        setDatas(await getAPI(url, failCb)); // FIXME: 수정 필요
-        successCb?.();
-      } catch (error: any) {
-        useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
-      } finally {
-        dispatch(useSetIsLoading({ isLoading: false }));
-      }
-    },
-    [datas],
-  );
-
-  return { datas, useGetDatas };
-}
-
-/**
- * [get method 커스텀 훅 (단일 값 호출)]
+ * [get method 커스텀 훅]
  *
  * @param {TypeGetAPIHookParams} params
  * url, API 성공시 바로 실행하는 콜백, API 실패시 바로 실행하는 콜백, 에러팝업 버튼 콜백을 담고 있는 객체
@@ -119,7 +60,7 @@ export function useGetDataHook({
 }: TypeGetAPIHookParams) {
   const dispatch = useDispatch();
   const useSetCatchClauseForErrorPopup = useSetCatchClauseForErrorPopupHook();
-  const [data, setData] = useState<any>(null); // FIXME: 수정 필요
+  const [data, setData] = useState<any>(null);
 
   useEffect(() => {
     (async () => {
@@ -127,7 +68,7 @@ export function useGetDataHook({
 
       try {
         dispatch(useSetIsLoading({ isLoading: true }));
-        setData(await getAPI(url, failCb)); // FIXME: 수정 필요
+        setData(await getAPI(url, failCb));
         successCb?.();
       } catch (error: any) {
         useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
@@ -135,7 +76,7 @@ export function useGetDataHook({
         dispatch(useSetIsLoading({ isLoading: false }));
       }
     })();
-  }, [url]);
+  }, []);
 
   const useGetData = useCallback(
     async ({
@@ -148,7 +89,7 @@ export function useGetDataHook({
 
       try {
         dispatch(useSetIsLoading({ isLoading: true }));
-        setData(await getAPI(url, failCb)); // FIXME: 수정 필요
+        setData(await getAPI(url, failCb));
         successCb?.();
       } catch (error: any) {
         useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
@@ -163,69 +104,7 @@ export function useGetDataHook({
 }
 
 /**
- * [post method 커스텀 훅 (배열 값 호출)]
- *
- * @param {TypeGetAPIHookParams} params
- * url, body 데이터, API 성공시 바로 실행하는 콜백,
- * API 실패시 바로 실행하는 콜백, 에러팝업 버튼 콜백을 담고 있는 객체
- *
- * @returns
- */
-export function usePostDatasHook({
-  url,
-  params,
-  successCb,
-  failCb,
-  errorPopupBtnCb,
-}: TypePostAPIHookParams) {
-  const dispatch = useDispatch();
-  const useSetCatchClauseForErrorPopup = useSetCatchClauseForErrorPopupHook();
-  const [datas, setDatas] = useState<any[]>([]); // FIXME: 수정 필요
-
-  useEffect(() => {
-    (async () => {
-      if (!url) return;
-
-      try {
-        dispatch(useSetIsLoading({ isLoading: true }));
-        setDatas(await postAPI(url, params, failCb)); // FIXME: 수정 필요
-        successCb?.();
-      } catch (error: any) {
-        useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
-      } finally {
-        dispatch(useSetIsLoading({ isLoading: false }));
-      }
-    })();
-  }, [url]);
-
-  const usePostDatas = useCallback(
-    async ({
-      url,
-      params,
-      successCb,
-      failCb,
-      errorPopupBtnCb,
-    }: TypePostAPIHookParams) => {
-      if (!url) return;
-
-      try {
-        dispatch(useSetIsLoading({ isLoading: true }));
-        setDatas(await postAPI(url, params, failCb)); // FIXME: 수정 필요
-        successCb?.();
-      } catch (error: any) {
-        useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
-      } finally {
-        dispatch(useSetIsLoading({ isLoading: false }));
-      }
-    },
-    [datas],
-  );
-
-  return { datas, usePostDatas };
-}
-
-/**
- * [post method 커스텀 훅 (단일 값 호출)]
+ * [post method 커스텀 훅]
  *
  * @param {TypeGetAPIHookParams} params
  * url, body 데이터, API 성공시 바로 실행하는 콜백,
@@ -242,7 +121,7 @@ export function usePostDataHook({
 }: TypePostAPIHookParams) {
   const dispatch = useDispatch();
   const useSetCatchClauseForErrorPopup = useSetCatchClauseForErrorPopupHook();
-  const [data, setData] = useState<any>(null); // FIXME: 수정 필요
+  const [data, setData] = useState<any>(null);
 
   useEffect(() => {
     (async () => {
@@ -250,7 +129,7 @@ export function usePostDataHook({
 
       try {
         dispatch(useSetIsLoading({ isLoading: true }));
-        setData(await postAPI(url, params, failCb)); // FIXME: 수정 필요
+        setData(await postAPI(url, params, failCb));
         successCb?.();
       } catch (error: any) {
         useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
@@ -258,7 +137,7 @@ export function usePostDataHook({
         dispatch(useSetIsLoading({ isLoading: false }));
       }
     })();
-  }, [url]);
+  }, []);
 
   const usePostData = useCallback(
     async ({
@@ -272,7 +151,7 @@ export function usePostDataHook({
 
       try {
         dispatch(useSetIsLoading({ isLoading: true }));
-        setData(await postAPI(url, params, failCb)); // FIXME: 수정 필요
+        setData(await postAPI(url, params, failCb));
         successCb?.();
       } catch (error: any) {
         useSetCatchClauseForErrorPopup(error, errorPopupBtnCb);
